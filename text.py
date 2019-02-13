78865272f21c4c8a2e286bea3937f9fe0a79d90a
NoNamesUserName <46601892+NoNamesUserName@users.noreply.github.com>
2019-02-13 09:04:50 -0600

    Update text.py

--- text.py
+++ text.py
@@ -14,6 +14,7 @@ The number of 4 letter words in the file
 The number of each of the vowels in the file
 The longest word in the file
 """
+
 """
 open/close.txt
 """
@@ -22,7 +23,7 @@ def opent():
 def close():
     txtf.close()
 """
-open/close.txt
+open/close.txt/split
 """
 
 """
@@ -41,32 +42,85 @@ of lines
 """
 num of words
 """
+def numw():
+    
+    txtf=opent()
+    txt = txtf.read().split()
+    return (len(txt))
+
+
+close()
 
 
 """
 num of words
 """
-txtf=opent()
-txt = txtf.read().split()
 
+"""
+number of each vowel
+"""
+countA=0
+countE=0
+countI=0
+countO=0
+countU=0
+countALL=0
+
+aA="a"
+eE="e"
+iI="i"
+oO="o"
+uU="u"
+
+txtf=opent()
+x=0 
+ntxt=len(txtf.read())
 close()
+
+txtf=opent()
+while x <= ntxt:
+    x+=1
+    newtx=txtf.read(1).lower()
+      
+    if aA == newtx:
+        countA+=1
+    elif eE in newtx:
+        countE+=1
+    elif iI in newtx:
+        countI+=1
+    elif oO in newtx:
+        countO+=1
+    elif uU in newtx:
+        countU+=1
+countALL=countA+countE+countI+countO+countU
+    
+close()
+print (countA)
+print (countE)
+print (countI)
+print (countO)
+print (countU)
+print (countALL)
 """
 number of each vowel
 """
 
-aA=set("a A")
-eE=("e","E")
-iI=("i","I")
-oO=("o","O")
-uU=("u","U")
+"""
+num of 4 letter words
+"""
+def fl():
+    txtf=opent()
+    txt = txtf.read().split()
+    return (nfl)
+    close()
+"""
+num of 4 letter words
+"""
 
-txtf=opent()
-txt = txtf.read().split()
+"""
+longest word
+"""
 
-countA=0
-
-for at in txt:
-    if at in txt:
-        countA+=1
-        print(countA)
-close()
+"""
+longest word
+"""
