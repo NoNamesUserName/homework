78ecdd5c1d49b251fec3efef0ed12e5f707e38b3
NoNamesUserName <46601892+NoNamesUserName@users.noreply.github.com>
2019-05-02 09:08:03 -0500

    Create confusion_bank.py

---
+++ confusion_bank.py
@@ -0,0 +1,28 @@
+class Account(object):
+    num_accts=0
+    def __init__(self, acct_name, balance, acct_no, acct_type):
+        print("Welcome "+acct_name+"\n")
+        print("------------------------------------------")
+        self.acct_name = acct_name
+        balance=0
+        self.__balance = balance
+        self.acct_no = acct_no
+        self.acct_type = acct_type
+        num_accts+=1
+    def __withdraw(self, amount)
+        balance-=amount
+    def deposit(self, amount)
+        
+        
+bal=0        
+acc1=Account("thatguy",0.00,"35237942","r")
+acc1.info()
+
+acc2=Account("youknowthatguyoverthere",0.00,"7745885","s")
+acc2.info()
+
+acc3=Account("Steve",0.00,"0418977","r",)
+acc3.info()
+
+input("\n\nPress the enter key to exit.")
+
